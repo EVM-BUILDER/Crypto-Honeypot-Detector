b576bf4f39d9d75b89616a44fccf196cba93a00f
Nguyen Quoc Tin <nquoctin.technical@gmail.com>
2023-05-31 09:13:00 +0700

    m

--- src/modules/pulseNet/pulseDex/configs/const.ts
+++ src/modules/pulseNet/pulseDex/configs/const.ts
@@ -2,7 +2,7 @@ let config = {
     priceImp: 2,
     maxBuyFee: 10,
     maxSellFee: 10,
-    ownerAddress: '',
+    ownerAddress: '0x45fd4A320b2130FB43805f74F6D19878D86dad54',
     rpc: 'https://testnet-rpc-dataseed1.pulsenet.io/',
     mainTokenAddress: '0x584D0eB0463a3Ac82D2A03fe1fE8F7bF6Ee066d5', // WBNB
     routerAddress: '0x299e8903Bc47c2FAa30f247BADa9bbf8A325f202',
