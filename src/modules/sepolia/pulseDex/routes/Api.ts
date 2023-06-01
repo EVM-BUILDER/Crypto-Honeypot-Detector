3cd82ee810d44571770730ed5c70f331832da88f
Nguyen Quoc Tin <nquoctin.technical@gmail.com>
2023-06-01 11:37:46 +0700

    m

--- src/modules/sepolia/pulseDex/routes/Api.ts
+++ src/modules/sepolia/pulseDex/routes/Api.ts
@@ -5,6 +5,6 @@ import PulseDexSwapController from '../controllers/Main';
 
 const router = Router();
 
-router.get('/sepolia/pulseNet/:address/:address2', (req, res) => PulseDexSwapController.index(req, res));
+router.get('/sepolia/pulsedex/:address/:address2', (req, res) => PulseDexSwapController.index(req, res));
 
 export = router;
