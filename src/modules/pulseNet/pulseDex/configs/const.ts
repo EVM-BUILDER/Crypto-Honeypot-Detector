c27aad48fcbcd2110b6e3ce2f8ed2cdfbd51fbc7
Nguyen Quoc Tin <nquoctin.technical@gmail.com>
2023-07-19 20:31:39 +0700

    m

--- src/modules/pulseNet/pulseDex/configs/const.ts
+++ src/modules/pulseNet/pulseDex/configs/const.ts
@@ -4,10 +4,10 @@ let config = {
     maxSellFee: 10,
     ownerAddress: '0x45fd4A320b2130FB43805f74F6D19878D86dad54',
     rpc: 'https://testnet-rpc-dataseed1.pulsenet.io/',
-    mainTokenAddress: '0x584D0eB0463a3Ac82D2A03fe1fE8F7bF6Ee066d5', // WBNB
-    routerAddress: '0xaF2deb4F2Eda4ABcE3B98Ef62fF62137CA1034AB',
-    multicallAddress: '0x0D9784937484e888546B8199672f58a6FB1449eE',
-    lockerAddress: '0x2219e1239a3B69fe78AF2a0De7d594F12442F9Be',
+    mainTokenAddress: '0x7BD296212272f4cB835c0c5688E942Fef4C3dA5f', // WBNB
+    routerAddress: '0xD00FF5E2a68A37aF6a92F72E6502483b8b14B9A5',
+    multicallAddress: '0xc036BB347b6Bb7e4098193B62CACb0c14Ee84BEF',
+    lockerAddress: '0xE0D27F690e95DF05bBD425C79036Fe3d6b0E5EF3',
     mainTokentoSell: '0.001',
     maxgas: 2000000,
     minMain: 4,
