8f7d5b7dd10eb1c45bba4b47b9e02f8263d66bdf
Nguyen Quoc Tin <nquoctin.technical@gmail.com>
2023-08-04 14:36:32 +0700

    m

--- src/modules/pulseNet/pulseDex/configs/const.ts
+++ src/modules/pulseNet/pulseDex/configs/const.ts
@@ -5,7 +5,7 @@ let config = {
     ownerAddress: '0x45fd4A320b2130FB43805f74F6D19878D86dad54',
     rpc: 'https://testnet-rpc-dataseed1.pulsenet.io/',
     mainTokenAddress: '0x7BD296212272f4cB835c0c5688E942Fef4C3dA5f', // WBNB
-    routerAddress: '0xD00FF5E2a68A37aF6a92F72E6502483b8b14B9A5',
+    routerAddress: '0x93A852dB2Cf34d73bC1918e4c8e06C8c133FBaDd',
     multicallAddress: '0xc036BB347b6Bb7e4098193B62CACb0c14Ee84BEF',
     lockerAddress: '0xE0D27F690e95DF05bBD425C79036Fe3d6b0E5EF3',
     mainTokentoSell: '0.001',
