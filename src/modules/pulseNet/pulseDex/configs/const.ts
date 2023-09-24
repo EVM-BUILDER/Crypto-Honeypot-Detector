6a6582d477378ad966eb858437e1676da2cbd793
Vincent Cone <vinhphuc591@gmail.com>
2023-09-24 15:39:58 +0700

    update dex contract for router, locker

--- src/modules/pulseNet/pulseDex/configs/const.ts
+++ src/modules/pulseNet/pulseDex/configs/const.ts
@@ -4,10 +4,10 @@ let config = {
     maxSellFee: 10,
     ownerAddress: '0x45fd4A320b2130FB43805f74F6D19878D86dad54',
     rpc: 'https://testnet-rpc-dataseed1.pulsenet.io/',
-    mainTokenAddress: '0x7BD296212272f4cB835c0c5688E942Fef4C3dA5f', // WBNB
-    routerAddress: '0x93A852dB2Cf34d73bC1918e4c8e06C8c133FBaDd',
+    mainTokenAddress: '0x7BAf116fbcC67c1d83C850edff430dbAEf16Ac1e', // WBNB
+    routerAddress: '0x494E9B8F660Df6AE7E32ba4d5D723EC41D81E420',
     multicallAddress: '0xc036BB347b6Bb7e4098193B62CACb0c14Ee84BEF',
-    lockerAddress: '0xE0D27F690e95DF05bBD425C79036Fe3d6b0E5EF3',
+    lockerAddress: '0xdfE44Db61cbd2146004872185A417Aedf7c7dA21',
     mainTokentoSell: '0.001',
     maxgas: 2000000,
     minMain: 4,
